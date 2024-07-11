9e2a7c8d9c167c5836af41684e3659e9671adef3
Justin Vocke <therealmathhelper@gmail.com>
2024-07-11 01:37:16 -0400

    Add TransferDbContext and TransferLog model

--- MicroRabbit.Transfer.Api/Program.cs
+++ MicroRabbit.Transfer.Api/Program.cs
@@ -1,19 +1,46 @@
+using MicroRabbit.Infrastructure.IoC;
+using MicroRabbit.Transfer.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TransferDbContext>(
 
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("TransferDbConnection"))
+);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "v1",
+        Description = "Transfer Microservice"
+    });
+});
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+RegisterServices(builder.Services);
+
+void RegisterServices(IServiceCollection services)
+{
+    DependencyContainer.RegisterServices(services);
+}
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transfer Microservice V1");
+    });
 }
 
 app.UseHttpsRedirection();
