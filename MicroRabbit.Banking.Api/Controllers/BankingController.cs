a8c155ec224b3e01a87fa15a1aa56762f930a28a
Justin Vocke <therealmathhelper@gmail.com>
2024-07-10 23:05:46 -0400

    Add commands and command handlers to create a transfer and publish to TransferCreatedEvent queue in RabbitMQ.

--- MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -1,4 +1,5 @@
 using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,6 +23,13 @@ namespace MicroRabbit.Banking.Api.Controllers
         {
            
             return Ok(_accountService.GetAccounts());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        {
+            _accountService.Transfer(accountTransfer);
+            return Ok();
         }
     }
 }
