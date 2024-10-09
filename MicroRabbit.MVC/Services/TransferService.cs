7a343cb280754c428480d61bd28de4f7dfa23c81
Justin Vocke <therealmathhelper@gmail.com>
2024-10-08 21:30:36 -0500

    add controller and mvc views

---
+++ MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,25 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient httpClient)
+        {
+
+            _apiClient = httpClient;
+        }
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:7058/api/banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto), 
+                System.Text.Encoding.UTF8, "application/json");  
+
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
